0ceaac569be192ec9f120b3631d82d28f9cd7aa0
dpdnjs1222 <dpdnjs1222@naver.com>
2020-04-04 10:41:37 +0900

    study : add folder

---
+++ guide1/src/main/java/com/example/guide1/Guide1Application.java
@@ -0,0 +1,13 @@
+package com.example.guide1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Guide1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Guide1Application.class, args);
+	}
+
+}
---
+++ guide1/src/test/java/com/example/guide1/Guide1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guide1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Guide1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
